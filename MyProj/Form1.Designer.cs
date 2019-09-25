f2e2e8e4a5b3610f6793c45d85ddbecce3dde666
Philip Åsberg Swenneke <philip.asberg-swenneke@sogeti.com>
2019-09-25 14:29:27 +0200

    buttonhandler

--- MyProj/Form1.Designer.cs
+++ MyProj/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            buttonhandler buttonhandlerclick = new buttonhandler();
+
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 310);
+            this.button1.Name = "yesButton";
+            this.button1.Size = new System.Drawing.Size(117, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(buttonhandlerclick.button_clicked);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 310);
+            this.button2.Name = "noButton";
+            this.button2.Size = new System.Drawing.Size(117, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "No";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(buttonhandlerclick.button_clicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ MyProj/buttonhandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyProj
+{
+    class buttonhandler
+    {
+        public void button_clicked(object sender, EventArgs e)
+        {
+            
+            
+            switch ((sender as Button).Name)
+            {
+                case "yesButton":
+                    MessageBox.Show("You clicked yes");
+                    break;
+
+                case "noButton":
+                    MessageBox.Show("You clicked no");
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
